3dbb993fb082c898a3b0af80de1be1723a4f3e36
eshanme <shantanu.mendhekar@ericsson.com>
2018-11-17 12:53:58 -0600

    Testing URL fragment support

--- src/components/School/Python_Basics.vue
+++ src/components/School/Python_Basics.vue
@@ -1,21 +1,60 @@
 <template>
   <div class="rmarkdown">
 
-    <iframe src="../../static/Python_Basics.html" frameborder="0"
-    style="overflow:hidden; overflow-x:hidden; overflow-y:hidden; height:94%; width:100%; position:absolute; top:50px; left:0px; right:0px; bottom:0px"
-    height="100%" width="100%"/>
-    
-  </div> 
+    <iframe
+    id="myIframe"
+    :src="iframe.src"
+    frameborder="0"
+    v-on:load="load()"
+    v-show="iframe.loaded"
+    style="overflow:hidden; overflow-x:hidden; overflow-y:hidden; height:90%; width:100%; position:absolute; top:50px; left:0px; right:0px; bottom:0px"
+    height="100%"
+    width="100%"/>
+
+
+  </div>
 </template>
 
 <script>
+
 export default {
-  name: 'Python_Basics'
+  name: 'Python_Basics',
+  data () {
+    return {
+      iframe: { src: '../../static/iFrame.html', loaded: false }
+    }
+  },
+  mounted: function () {
+    // Listen to message from iframe element
+    window.addEventListener('message', function (msg) {
+      var message = msg.data
+      var locationHash = message.substring(message.indexOf('#'))
+      // console.log(locationHash)
+
+      if (message.startsWith('locHash:')) {
+        var newurl = window.location.protocol + '//' + window.location.host + window.location.pathname + locationHash
+        window.history.pushState({ path: newurl }, '', newurl)
+      }
+    })
+  },
+  methods: {
+    load: function () {
+      this.iframe.loaded = true
+      var hash = window.location.hash
+
+      if (hash) {
+        document.getElementById('myIframe').contentWindow.postMessage(hash, '*')
+        console.log(hash)
+      } else {
+        console.log('No hash!')
+      }
+    }
+  }
 }
 </script>
 
 <style scoped>
-html{
+html {
   overflow: hidden;
 }
 
